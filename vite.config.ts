5e80db7d72b61b06d9e17a131272e0d76ee0002a
Karol Kotański <karolfm12@onet.eu>
2024-10-24 14:30:19 +0200

    ok6

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { createStaticCopy } from "vite-plugin-static-copy";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    createStaticCopy({
+      targets: [
+        {
+          src: "assets/images/*",
+          dest: "assets/images", // gdzie kopiować w folderze dist
+        },
+      ],
+    }),
+  ],
   base: "/product-list-with-cart-main2/",
 });
